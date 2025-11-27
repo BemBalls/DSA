958bf6007903d953ac3bfdd174c2d0f35b14a705
BemBalls <11820033@usc.edu.ph>
2025-11-28 01:53:46 +0800

    tree

--- 12-Graphs/DFS.c
+++ 12-Graphs/DFS.c
@@ -4,7 +4,6 @@
 #define unvisited 100
 #define visited -1
 
-
 typedef struct node{
     int elem;
     struct node* link;
@@ -21,22 +20,25 @@ void dfs(Graph G, vertex v, int * marker){
     marker[v] = visited;
     printf("%d -> ", v);
     
-    Node pointer = G[v]
+    ptr pointer = G[v];
     while(pointer != NULL){
         vertex w = pointer->elem;
         if(marker[w] == unvisited){
             dfs(G, w, marker);
         }
-        pointer = pointer->link
+        pointer = pointer->link;
+        
     }
+    
+    
     
 }
 
 void insertLast(Graph G, vertex v, vertex w){
-    ptr newNode = (ptr)calloc(1,sizeof(node));
-    newNode->elem = v;
+    ptr newNode = (ptr)calloc(1,sizeof(Node));
+    newNode->elem = w;
 
-    if(G[w] == NULL) G[w] = newNode;
+    if(G[v] == NULL) G[v] = newNode;
     else{
         ptr temp = G[w];
         while(temp->link != NULL){
@@ -49,19 +51,19 @@ void insertLast(Graph G, vertex v, vertex w){
 
 int main(){
     Graph G;
-    int marker;
+    int marker[MAX];
+    vertex start = 2;
     for (int i = 0; i < MAX; i++) {
         G[i] = NULL;
         marker[i] = unvisited;
     }
-
-    insertLast(G, 3, 2);
-    insertLast(G, 2, 4);
+    
+    insertLast(G, 2, 3);
+    insertLast(G, 3, 4);
     insertLast(G, 4, 1);
     insertLast(G, 1, 5);
-    vertex start = 2;
-    dfs(G, v, &marker);
-
+    dfs(G, start, marker);
+    printf("NULL");
 }
 
 
